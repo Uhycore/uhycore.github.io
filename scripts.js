30aaac108a3b0ddd61b3d042fec83cbd1f8ec273
Uhycore <arilmubin0@gmail.com>
2025-02-12 18:48:44 +0700

    fastbots

--- scripts.js
+++ scripts.js
@@ -24,37 +24,37 @@ function sendMessage() {
     window.open(whatsappLink, '_blank');
 }
 
-// Chatbase Script
-(function () { 
-    if (!window.chatbase || window.chatbase("getState") !== "initialized") { 
-        window.chatbase = (...arguments) => { 
-            if (!window.chatbase.q) { 
-                window.chatbase.q = [] 
-            } 
-            window.chatbase.q.push(arguments) 
-        }; 
-        window.chatbase = new Proxy(window.chatbase, { 
-            get(target, prop) { 
-                if (prop === "q") { 
-                    return target.q 
-                } 
-                return (...args) => target(prop, ...args) 
-            } 
-        }) 
-    } 
-    const onLoad = function () { 
-        const script = document.createElement("script"); 
-        script.src = "https://www.chatbase.co/embed.min.js"; 
-        script.id = "39QWPWrjQZXG1A_4Mj_j6"; 
-        script.domain = "www.chatbase.co"; 
-        document.body.appendChild(script) 
-    }; 
-    if (document.readyState === "complete") { 
-        onLoad() 
-    } else { 
-        window.addEventListener("load", onLoad) 
-    } 
-})();
+// // Chatbase Script
+// (function () { 
+//     if (!window.chatbase || window.chatbase("getState") !== "initialized") { 
+//         window.chatbase = (...arguments) => { 
+//             if (!window.chatbase.q) { 
+//                 window.chatbase.q = [] 
+//             } 
+//             window.chatbase.q.push(arguments) 
+//         }; 
+//         window.chatbase = new Proxy(window.chatbase, { 
+//             get(target, prop) { 
+//                 if (prop === "q") { 
+//                     return target.q 
+//                 } 
+//                 return (...args) => target(prop, ...args) 
+//             } 
+//         }) 
+//     } 
+//     const onLoad = function () { 
+//         const script = document.createElement("script"); 
+//         script.src = "https://www.chatbase.co/embed.min.js"; 
+//         script.id = "39QWPWrjQZXG1A_4Mj_j6"; 
+//         script.domain = "www.chatbase.co"; 
+//         document.body.appendChild(script) 
+//     }; 
+//     if (document.readyState === "complete") { 
+//         onLoad() 
+//     } else { 
+//         window.addEventListener("load", onLoad) 
+//     } 
+// })();
 
 // Custom Tailwind configuration for more vibrant colors
 tailwind.config = {
